013a26b802f24135298affc492f0174672439bd3
rk2609 <accounts@wanderon.in>
2020-09-05 08:20:02 +0530

    Removed Title

--- pages/property/[slug].js
+++ pages/property/[slug].js
@@ -34,7 +34,6 @@ const Property = ({ property }) => {
   return (
     <Layout>
       <Head>
-        <title>{property.title}</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
