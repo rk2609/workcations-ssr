5f2e323387b1569ba20829b38dbd86895ed092bb
rk2609 <accounts@wanderon.in>
2020-09-20 14:06:46 +0530

    Gallery

--- components/gallery/gallery.jsx
+++ components/gallery/gallery.jsx
@@ -67,6 +67,52 @@ const Gallery = ({ images, slug, loadElements }) => {
           </svg>
         </GalleryArrow>
       </HeroImage>
+      <ThumbnailContainer>
+        <ArrowContainer onClick={previousThumbnail}>
+          <svg viewBox="0 0 32 32" viewBox="0 0 32 32" aria-hidden="true">
+            <path d="M14.19 16.005l7.869 7.868-2.129 2.129-9.996-9.997L19.937 6.002l2.127 2.129z" />
+          </svg>
+        </ArrowContainer>
+
+        <Thumbnails ref={thumbnailContainer} className="thumbnail-container">
+          {images.map((image, i) =>
+            i === currentSlide ? (
+              <div
+                key={image}
+                style={{
+                  backgroundImage: `url(https://www.wanderon.in/workcations/${slug}/${image}.jpg)`,
+                }}
+                onClick={() => {
+                  activeSlide(i);
+                }}
+              ></div>
+            ) : (
+              <div
+                key={image}
+                style={{
+                  backgroundImage: `linear-gradient(to top,rgba(0,0,0,0.45) 0%,rgba(0,0,0,0.45) 100%),url(https://www.wanderon.in/workcations/${slug}/${image}.jpg)`,
+                }}
+                onClick={() => {
+                  activeSlide(i);
+                }}
+              ></div>
+            )
+          )}
+        </Thumbnails>
+        <ArrowContainer onClick={nextThumbnail}>
+          <svg viewBox="0 0 32 32" viewBox="0 0 32 32" aria-hidden="true">
+            <path d="M18.629 15.997l-7.083-7.081L13.462 7l8.997 8.997L13.457 25l-1.916-1.916z" />
+          </svg>
+        </ArrowContainer>
+      </ThumbnailContainer>
+    </Container>
+  );
+};
+
+export default Gallery;
+
+/*
+
       {loadElements ? (
         <ThumbnailContainer>
           <ArrowContainer onClick={previousThumbnail}>
@@ -107,8 +153,5 @@ const Gallery = ({ images, slug, loadElements }) => {
           </ArrowContainer>
         </ThumbnailContainer>
       ) : null}
-    </Container>
-  );
-};
 
-export default Gallery;
+      */
